46b36e02b0298e5ad3b873e4bcd674e64ce41612
Mauricio Rodriguez <mauricio.rodriguez@xintec.cl>
2021-05-24 20:35:02 -0400

    add: logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get, HttpStatus, Param, ParseIntPipe, Res } from "@nestjs/common";
 import { RedisService } from './redis/redis.service';
+import { LoggerService } from './logger/logger.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly _redisService: RedisService) {}
+  constructor(
+    private readonly _redisService: RedisService,
+    private readonly _loggerService: LoggerService
+    ){}
 
   @Get(':idKey')
   async getRedis(@Param('idKey', ParseIntPipe) idKey: number, @Res() res){
     const dataRedis: any = await this._redisService.getRedis(idKey);
 
     if (dataRedis === null){
+      this._loggerService.customError({}, {message: "=> Data Not Found"});
       return res.status(HttpStatus.NOT_FOUND).json(`${res.statusCode} - Data Not Found: Key => ${idKey} `);
     } else {
+        this._loggerService.customInfo({}, {message: `finished => ${idKey}`});
         return res.status(HttpStatus.OK).json(dataRedis);
     }
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { RedisService } from './redis/redis.service';
 import { ConfigModule } from './config/config.module';
+import { LoggerService } from './logger/logger.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AppController],
-  providers: [RedisService],
+  providers: [RedisService, LoggerService],
 })
 
 export class AppModule {}
